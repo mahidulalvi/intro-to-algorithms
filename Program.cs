d2170c0bd7d5d5e7758bfb2c1ca21f2353e46c5c
Md Mahidul Haque Alvi <mdmahidulhaque001@gmail.com>
2021-04-08 07:45:44 -0500

    Initital commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intro_to_algorithms_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's practise some algorithms!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# intro_to_algorithms_practice
+
+A console application to practise & analyze the algorithms in Intro to Algorithms, 3rd edition.
